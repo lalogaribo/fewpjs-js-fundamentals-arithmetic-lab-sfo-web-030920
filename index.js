24705d19a5196b5d842bf273d837d6104e94da14
Edward Mendoza <lalo.garibo90@gmail.com>
2020-04-24 11:05:53 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-
 // Write your code here
+const num1 = 62,
+  num2 = 1;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10);
+const mod = 24 % 10;
+let max = Math.max(1, 3, 20);
